d2c8e2e9c51be7726e4a2270118ea1d89790a0df
mauritsWouters <wouters.maurits@gmail.com>
2020-09-16 15:42:51 +0200

    1ste poging 360 video
    1ste poging 360 video

--- eindwerk - Vilvoorde/eindwerk - Vilvoorde - hero v2/javascript.js
+++ eindwerk - Vilvoorde/eindwerk - Vilvoorde - hero v2/javascript.js
@@ -1,22 +1,49 @@
-var e = document.getElementById("myVideo");
-e.style.opacity = 0;
-
-function onClickButton() {
+document.addEventListener("DOMContentLoaded", function () {
   var vid = document.getElementById("myVideo");
-  vid.oncanplaythrough = function () {
-    setTimeout(function () {
-      var e = document.getElementById("myVideo");
-      fade(e);
-    }, 50);
+  var start = false;
+  vid.onloadstart = function () {
+    console.log("onloadstart");
+    start = true;
   };
+  vid.style.opacity = 0.0;
 
+  document.getElementById("button").addEventListener("click", onClickButton);
+
+  function onClickButton() {
+    console.log("click1");
+
+    var y = document.getElementById("FadeIn");
+    if (y.style.display === "none") {
+      y.style.display = "block";
+    }
+
+    var x = document.getElementById("onePager");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+    /*
+    console.log("click2");
+    vid.play();
+    if (start == true) {
+      console.log("load");
+      setTimeout(function () {
+        var e = document.getElementById("myVideo");
+        console.log("setT");
+        fade(e);
+      }, 50);
+    }*/
+  }
+  /*
   function fade(element) {
     var op = 0;
     var timer = setInterval(function () {
       if (op >= 1) clearInterval(timer);
+      console.log("setInt");
       element.style.opacity = op;
       element.style.filter = "alpha(opacity=" + op * 100 + ")";
       op += op * 0.1 || 0.1;
     }, 50);
-  }
-}
+  }*/
+});
